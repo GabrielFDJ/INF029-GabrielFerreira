ba2f7a08f20946dd095fc66ae23183f8388bc1f5
Gabriel <gabrielferreiradj2014@gmail.com>
2021-03-03 20:24:34 -0300

    At02

--- Atividades/AT02/testecompilador.c
+++ Atividades/AT02/testecompilador.c
@@ -1,49 +1,45 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 /*Atv 2 de laboratório */
 int main()
 {
-    int num1,num2;
+    float num1,num2;
     int aux = 0;
     int cont = 0;
     int acao;
-    int result;
+    float result = 0;
     
     printf("Digite dois numeros : ");
-    scanf("%d %d",&num1,&num2);
+    scanf("%f %f",&num1,&num2);
     
-    while (aux == 0) {
-        printf("\nModifique os numeros!!\n'"
+    printf("\nModifique os numeros!!\n'"
         "Digite apenas numeros positivos***\nDigite 0 para Sair do programa"
         "\nDigite 1 para Somar\nDigite 2 para Subtrair\nDigite 3 para multiplicar\n"
         "Digite 4 para dividir\nDigita 5 para alterar os numeros\n");
+        
+    while (aux == 0) {
+        printf("Opcao : ");
         scanf("%d",&acao);
         
         if(acao == 0){
             aux = 1;
         }
-        else if(acao == 1){
+        else if(acao == 1){ /* Soma */
             result = num1 + num2;
         }
-        else if(acao == 2){
+        else if(acao == 2){ /* Subtracao */
             result = num1 - num2;
         }
-        else if(acao == 3){
+        else if(acao == 3){ /* Multiplicacao */
             result = num1 * num2;
         }
-        else if(acao == 4){
+        else if(acao == 4){ /* Divisao */
             result = num1 / num2;
+            if (num1 == 0 || num2 == 0)
+            	printf("\nNao eh possivel dividir um numero por 0\nTente novamente , digitando 5 numeros para escolher outros numeros\n");            	
         }
-        else if(acao == 5){
-            printf("Digite dois numeros : ");
-            scanf("%d %d",&num1,&num2);
+        else if(acao == 5){ /* Digitar novamente */
+            printf("\nDigite dois numeros : ");
+            scanf("%f %f",&num1,&num2);
         }
         else
             printf("\nOpcao invalida ... Tente novamente \n");
@@ -51,13 +47,13 @@ int main()
         cont++;
         
         if(aux ==0)
-            printf("\nO resultado parcial eh : %d\n",result);
+            printf("\nO resultado parcial eh : %f\n",result);
     }
     
     if(cont == 1)
         printf("\nOs numeros nao foram alterados\nFim do programa");
     else
-        printf("Os numeros foram alterados\nO resultado final das operacoes foi : %d\nFim do programa",result);
+        printf("Os numeros foram alterados\nO resultado final das operacoes foi : %f\nFim do programa",result);
     
     return 0;
 }
