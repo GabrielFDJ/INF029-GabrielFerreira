aa9038e5e5398d4336377181a4da581a3e3b6ca5
Gabriel <gabrielferreiradj2014@gmail.com>
2021-03-24 12:31:41 -0300

    validacao do cadastro da atv08 + update

--- Atividades/Lista de funções/ATV09.c
+++ Atividades/Lista de funções/ATV09.c
@@ -23,23 +23,25 @@ int main(){
    
     cadastrarCliente(cd.nome, cd.dataNascimento, cd.cpf, cd.sexo);
     
+    printf("\n***************************************\n");
+    
     if(contN == 0)
-        printf("\nNome validado com SUCESSO = %s",cd.nome);
+        printf("\nNome = %s",cd.nome);
     else 
-        printf("\nErro de validacao no NOME");
+        printf("\nErro de validacao no NOME\n");
 	
 	if(contDT == 0)
-	    printf("\nData de nascimento validada com SUCESSO : %s",cd.dataNascimento);
+	    printf("Data de nascimento : %s",cd.dataNascimento);
 	else
-	    printf("\nErro de validacao na DATA");
+	    printf("Erro de validacao na DATA\n");
 	
 	if(contCP == 0)
-        printf("\nCPF validado com SUCESSO : %s",cd.cpf);
+        printf("Cpf : %s",cd.cpf);
     else
-        printf("\nErro de validacao no CPF");
+        printf("Erro de validacao no CPF");
 	
 	if(contS == 0)
-	   printf("\nSexo validado com SUCESSO = %c",cd.sexo); 
+	   printf("\nSexo = %c",cd.sexo); 
 	else 
 	    printf("\nErro de validacao no SEXO");
 	
@@ -70,17 +72,22 @@ struct cadastro cadastrarCliente(char nome[], char dataNascimento[], char cpf[],
 }
 	
 int validarNome(){
+    int i;
     int tamanho;
     tamanho = strlen(cd.nome);
     if(tamanho > 50)
-        contDT = 1;
+        contN = 1;
+    for(i=0;cd.nome[i] != '\0';i++){
+        if(cd.nome[i]-'0' >= 0 && cd.nome[i]-'0' < 10)
+            contN = 1;
+    }    
     return 0;
 }
 
 int validarNascimento(){
     int i;
     int tamanho;
-    int ano,bissexto = 0, mil,cent,dez,und;
+    int dia,mes,ano,bissexto = 0;
     tamanho = strlen(cd.dataNascimento);
     if((tamanho - 1) != 10){ // se o tamanho do nome esta correto
         contDT = 1;
@@ -95,80 +102,50 @@ int validarNascimento(){
                     contDT = 1;}
         }
     }
+    //calculo do dia
+    dia = (cd.dataNascimento[0]-'0') * 10;
+    dia += cd.dataNascimento[1]-'0';
+    //calculo do mes
+    mes = (cd.dataNascimento[3]-'0') * 10;
+    mes += cd.dataNascimento[4]-'0';
+    //calculo do ano 
+    ano = (cd.dataNascimento[6]-'0') * 1000;
+    ano += (cd.dataNascimento[7]-'0') * 100;
+    ano += (cd.dataNascimento[8]-'0') * 10;
+    ano += cd.dataNascimento[9]-'0';
     
-    for(i=0;i<10;i++){
-        if(i == 0 || i == 1){ // verifica se o dia é entre 1 e 31
-            if(cd.dataNascimento[0]-'0' > 3)
-                contDT = 1;
-            if(cd.dataNascimento[0]-'0' == 3 && cd.dataNascimento[1]-'0' > 1)
-                contDT = 1;
-        }
-    }
+    // verifica se o dia é entre 1 e 31.
+    if(dia < 1  || dia > 31)
+        contDT = 1;
     
-    for(i=0;i<10;i++){
-        if(i == 3 || i == 4){ // verifica se o mês é entre 1 e 12
-            if(cd.dataNascimento[3]-'0' > 1 )
-                contDT = 1;
-            if (cd.dataNascimento[3]-'0' >= 1 && cd.dataNascimento[4]-'0' > 2)
-                contDT = 1;
-        }
-    }
+    // verifica se o mês é entre 1 e 12    
+    if(mes < 1 || mes > 12)
+        contDT = 1;
     
-    for(i=0;i<10;i++){
-        if(i == 6 || i == 7  || i == 8 || i == 9){ // verifica se o ano esta no intervalo entre 1900 e 2100
-            if(cd.dataNascimento[6]-'0' > 3 )
-                contDT = 1;
-            if (cd.dataNascimento[6]-'0' == 1 && cd.dataNascimento[7]-'0' < 9)
-                contDT = 1;
-            if (cd.dataNascimento[6]-'0' == 2 && cd.dataNascimento[7]-'0' > 1)
-                contDT = 1;    
-            if (cd.dataNascimento[6]-'0' == 2 && cd.dataNascimento[7]-'0' == 1){
-                if(cd.dataNascimento[8]-'0' > 0  || cd.dataNascimento[9]-'0' > 0)
-                    contDT = 1;
-            }
-        }
-    }
+    // verifica se o ano esta no intervalo entre 1900 e 2100
+    if(ano < 1900 || ano > 2100)
+        contDT = 1;
     
-    for(i=0;i<10;i++){
-        if(i == 3 || i == 4){ // verifica se mês for 04, 06, 09 ou 11, dia pode ser no máximo 30;
-            if(cd.dataNascimento[3]-'0' == 0  && cd.dataNascimento[4]-'0' == 4)
-                if(cd.dataNascimento[0]-'0' == 3  && cd.dataNascimento[1]-'0' > 0)
-                    contDT = 1;
-            if(cd.dataNascimento[3]-'0' == 0  && cd.dataNascimento[4]-'0' == 6)
-                if(cd.dataNascimento[0]-'0' == 3  && cd.dataNascimento[1]-'0' > 0)
-                    contDT = 1;
-            if(cd.dataNascimento[3]-'0' == 0  && cd.dataNascimento[4]-'0' == 9)
-                if(cd.dataNascimento[0]-'0' == 3  && cd.dataNascimento[1]-'0' > 0)
-                    contDT = 1;  
-            if(cd.dataNascimento[3]-'0' == 1  && cd.dataNascimento[4]-'0' == 1)
-                if(cd.dataNascimento[0]-'0' == 3  && cd.dataNascimento[1]-'0' > 0)
-                    contDT = 1;         
-            }
-    }
+    // verifica se mês for 04, 06, 09 ou 11, dia pode ser no máximo 30;
+    if(mes == 4 || mes == 6 || mes == 9 || mes == 11)
+        if(dia > 30)
+            contDT = 1;
     
     //calculo do ano bissexto
-    mil = cd.dataNascimento[6]-'0';
-    cent = cd.dataNascimento[7]-'0';
-    dez = cd.dataNascimento[8]-'0';
-    und = cd.dataNascimento[9]-'0';
-    
-    ano = (mil * 1000) + (cent * 100) + (dez * 10) + und;
-    
     if(ano % 4 == 0 && ano % 100 != 0)
         bissexto = 1;
     if(ano % 4 == 0 && ano % 400 == 0 && ano % 100 == 0)
         bissexto = 1;   
     
     if(bissexto) { // verifica se ano for bissexto e mês for 02, dia pode ser no máximo 29.
-        if(cd.dataNascimento[3]-'0' == 0  && cd.dataNascimento[4]-'0' == 2)
-            if(cd.dataNascimento[0]-'0' > 2)
+        if(mes == 2)
+            if(dia > 29)
                 contDT = 1;
             }
     
-    else {
-        // verifica se mês for 02, dia pode ser no máximo 28;
-        if(cd.dataNascimento[3]-'0' == 0  && cd.dataNascimento[4]-'0' == 2)
-            if(cd.dataNascimento[0]-'0' == 2  && cd.dataNascimento[1]-'0' > 8)
+    else { // verifica se mês for 02, dia pode ser no máximo 28;
+        if(mes == 2)
+            if(dia > 28)
                 contDT = 1;
         }
     
@@ -203,28 +180,23 @@ int validarCpf(){
             if((cd.cpf [i]) != '.'){
                 soma1 += (cd.cpf[i]-'0') * aux1;
                 aux1 = aux1 - 1;}
-               
-             
     }  
     
     dg1 = cd.cpf[12]-'0'; // capturando o primero digito verificador
     valid1 = (soma1 * 10) % 11;
     
-    
-    
     if(valid1 == 10) // se o resto da divisão for igual a 10 só capturamos o 0 
         valid1 = 0;
     
-    
     if(valid1 != dg1) // verificando se o primeiro digito verificador é igual ao valid1 , primeira etapa de verificação concluida
         contCP = 1;
-    
         
     for(i=0;i<13;i++){ // validação do segundo digito
             if(cd.cpf [i] != '.' || cd.cpf [i] != '-')
                 soma2 += (cd.cpf[i]-'0') * aux2;
             aux2 = aux2 - 1; 
     }   
+    
     dg2 = cd.cpf[13]-'0'; // capturando o segundo digito verificador
     valid2 = (soma2 * 10) % 11;
     
@@ -246,6 +218,7 @@ int validarSexo(){
     return 0 ;
     
 }
+
 
 
 
